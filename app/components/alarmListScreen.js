c8b0921a03307adc110c36fc96056bd6f702fe03
Ticknor, Samantha <TicknorS@CarnegieMuseums.Org>
2017-02-28 13:59:19 -0500

    Give bird a little more headroom on smaller screen size

--- app/components/alarmListScreen.js
+++ app/components/alarmListScreen.js
@@ -155,7 +155,7 @@ const AlarmListScreen = (props) => {
           </TouchableOpacity>
         </View>
       );
-      messageHeight = 235;
+      messageHeight = 240;
     } else if (Platform.OS === 'android') {
       messageHeight = 180;
     }
