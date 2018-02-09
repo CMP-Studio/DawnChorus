e9f1911276bbb1d5a26f7e8ed88bb8e6851d5dee
Ticknor, Samantha <TicknorS@CarnegieMuseums.Org>
2018-02-09 12:28:26 -0500

    Managed modules, removed unused

--- android/app/src/main/java/com/dawnchorus/MainActivity.java
+++ android/app/src/main/java/com/dawnchorus/MainActivity.java
@@ -1,9 +1,21 @@
 package com.dawnchorus;
 
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import com.cboy.rn.splashscreen.SplashScreen;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import android.graphics.Color;
+
+import android.app.Activity;
+import android.util.Log;
+import javax.annotation.Nullable;
+
 
 public class MainActivity extends ReactActivity {
-
+  
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -11,5 +23,57 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "DawnChorus";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Put splash screen up
+        SplashScreen.show(this, false);
+
+        super.onCreate(savedInstanceState);
+        final Window win = getWindow();
+        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+    }
+
+    public static class AlarmActivityDelegate extends ReactActivityDelegate {
+        private static final String ALARM_ID = "alarmID";
+        private static final String MISSED_ALARMS = "missedAlarms";
+        private Bundle mInitialProps = null;
+        private final @Nullable Activity mActivity;
+
+        public AlarmActivityDelegate(Activity activity, String mainComponentName) {
+            super(activity, mainComponentName);
+            this.mActivity = activity;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            mInitialProps = new Bundle();
+            // bundle is where we put our alarmID with launchIntent.putExtra
+            Bundle bundle = mActivity.getIntent().getExtras();
+            if (bundle != null && bundle.containsKey(ALARM_ID)) {
+                // put any initialProps here
+                mInitialProps.putString(ALARM_ID, bundle.getString(ALARM_ID));
+            }
+            if (bundle != null && bundle.containsKey(MISSED_ALARMS)) {
+                // put any initialProps here
+                mInitialProps.putString(MISSED_ALARMS, bundle.getString(MISSED_ALARMS));
+            }
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        protected Bundle getLaunchOptions() {
+            return mInitialProps;
+        }
+    };
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new AlarmActivityDelegate(this, getMainComponentName());
     }
 }
--- android/app/src/main/java/com/dawnchorus/MainApplication.java
+++ android/app/src/main/java/com/dawnchorus/MainApplication.java
@@ -4,14 +4,17 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.dawnchorus.alarms.AlarmPackage;
+import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.jadsonlourenco.RNShakeEvent.RNShakeEventPackage;
+import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.zmxv.RNSound.RNSoundPackage;
+import com.tapme.RNVolume.VolumePackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.dawnchorus.androidnotificationpermissions.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,25 +22,27 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+      protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new SplashScreenReactPackage(),
+        new RNShakeEventPackage(),
+        new RNSoundPackage(),
+        new VolumePackage(),
+        new AlarmPackage(),
+        new ReactNativePushNotificationPackage()
+      );
+    }
+  };
+
   @Override
-  protected boolean getUseDeveloperSupport() {
-  return BuildConfig.DEBUG;
-}
-
-@Override
-protected List<ReactPackage> getPackages() {
-return Arrays.<ReactPackage>asList(
-  new MainReactPackage(),
-  new RNShakeEventPackage(),
-  new RNSoundPackage(),
-  new ReactNativePushNotificationPackage(),
-  new NotificationsPermissionsPackage()
-);
-}
-};
-
-@Override
-public ReactNativeHost getReactNativeHost() {
-return mReactNativeHost;
-}
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
 }
